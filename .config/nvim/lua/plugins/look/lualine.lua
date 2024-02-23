f0741aa67a7b18a966c6157b7ab1f6b19e01f5f3
Doeke <doekebuursma@gmail.com>
2024-02-22 22:04:32 -0500

    add virtual environment lualine component

--- .config/nvim/lua/plugins/look/lualine.lua
+++ .config/nvim/lua/plugins/look/lualine.lua
@@ -1,7 +1,14 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	dependencies = { "nvim-tree/nvim-web-devicons", "linux-cultist/venv-selector.nvim" },
 	opts = {
 		options = { theme = "auto" },
+		sections = {
+			lualine_y = {
+				function()
+					return require("venv-selector").get_active_venv() or "no virtual environment"
+				end,
+			},
+		},
 	},
 }
