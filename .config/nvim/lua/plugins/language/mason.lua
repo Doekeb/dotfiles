6a755bfd733f045fec2c9f09896b9a2391e42e25
Doeke <doekebuursma@gmail.com>
2024-03-01 10:10:48 -0500

    Change to mason tool installer for installing tools

--- .config/nvim/lua/plugins/language/mason.lua
+++ .config/nvim/lua/plugins/language/mason.lua
@@ -1,6 +1,12 @@
 return {
-	"williamboman/mason.nvim",
+	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
 	config = function()
 		require("mason").setup({})
+		require("mason-lspconfig").setup({})
+		local tools = {}
+		vim.list_extend(tools, { "lua_ls", "pylsp" }) -- lsp tools
+		vim.list_extend(tools, { "stylua", "mypy", "black", "isort", "flake8", "pylint" }) -- null_ls tools
+		require("mason-tool-installer").setup({ ensure_installed = tools })
 	end,
 }
