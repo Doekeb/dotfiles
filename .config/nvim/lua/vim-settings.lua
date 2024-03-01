5559df4ccf92fe8b9a911edb13228a430ff8c72c
doeke <doekebuursam@gmail.com>
2024-02-29 22:00:47 -0500

    Change to buffer local settings for lsp keymaps

--- .config/nvim/lua/vim-settings.lua
+++ .config/nvim/lua/vim-settings.lua
@@ -11,6 +11,5 @@ vim.diagnostic.config({ signs = false })
 -- vim.cmd.set("nnoremap <Space> <NOP>")
 
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>|", "<cmd>belowright vnew<cr>")
 vim.keymap.set("n", "<leader>_", "<cmd>belowright new<cr>")
