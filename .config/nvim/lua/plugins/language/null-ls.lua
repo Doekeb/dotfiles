7ece58dd7946233e9dc4db976b007e5922e9e8da
doeke <doekebuursam@gmail.com>
2024-03-11 00:19:18 -0400

    Default longer line length in flake8

--- .config/nvim/lua/plugins/language/null-ls.lua
+++ .config/nvim/lua/plugins/language/null-ls.lua
@@ -7,7 +7,7 @@ return {
 		local flake8 = require("none-ls.diagnostics.flake8")
 		null_ls.python_sources = {
 			-- null_ls.builtins.diagnostics.flake8, -- (Deprecated: https://github.com/nvimtools/none-ls.nvim/discussions/81)
-			flake8,
+			flake8.with({ extra_args = { "--max-line-length", "88" } }),
 			null_ls.builtins.diagnostics.mypy,
 			null_ls.builtins.diagnostics.pylint,
 		}
