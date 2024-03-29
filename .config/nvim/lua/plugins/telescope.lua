4ce1a645b82bd43ee39e20435ead51293c36a8bb
Doeke <doekebuursma@gmail.com>
2024-03-29 09:54:58 -0400

    Add jumplist shortcut to telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -31,6 +31,7 @@ return {
     vim.keymap.set("n", "<leader>ft", builtin.builtin, {}) -- [f]ind [t]elescope
     vim.keymap.set("n", "<leader>fe", builtin.diagnostics, {}) -- [f]ind [e]rrors
     vim.keymap.set("n", "<leader>fs", builtin.search_history, {}) -- [f]ind [s]earch history
+    vim.keymap.set("n", "<leader>fj", builtin.jumplist, {}) -- [f]ind [j]umps
 
     vim.keymap.set("n", "<leader>fgb", builtin.git_branches, {}) -- [f]ind [g]it [b]ranches
     vim.keymap.set("n", "<leader>fgc", builtin.git_commits, {}) -- [f]ind [g]it [c]ommits
