72e19f564386a85fcd67a66b649005641528a462
Doeke <doekebuursma@gmail.com>
2024-04-04 13:05:16 -0400

    Swap vim-surround with nvim-surround

--- .config/nvim/lua/plugins/treesitter/surround.lua
+++ .config/nvim/lua/plugins/treesitter/surround.lua
@@ -1,1 +1,6 @@
-return { "tpope/vim-surround" }
+return {
+  "kylechui/nvim-surround",
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  opts = {},
+}
