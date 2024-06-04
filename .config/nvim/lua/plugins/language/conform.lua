2656389f684320bdf08cdc0ab64f53c007e26fa4
Doeke <doekebuursma@gmail.com>
2024-06-04 09:54:45 -0400

    Config for sql_formatter

--- .config/nvim/lua/plugins/language/conform.lua
+++ .config/nvim/lua/plugins/language/conform.lua
@@ -1,16 +1,25 @@
 return {
   "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      javascript = { "biome" },
-      javascriptreact = { "biome" },
-      json = { "biome" },
-      lua = { "stylua" },
-      python = { "isort", "black" },
-      sql = { "sql_formatter" },
-      typescript = { "biome" },
-      typescriptreact = { "biome" },
-    },
-    format_on_save = { lsp_fallback = true },
-  },
+  config = function()
+    local conform = require("conform")
+    conform.formatters.sql_formatter = {
+      prepend_args = {
+        "-c",
+        '{"language": "postgresql", "dialect": "postgresql", "tabWidth": 2, "useTabs": false, "keywordCase": "lower", "dataTypeCase": "lower", "functionCase": "lower", "logicalOperatorNewline": "before", "linesBetweenQueries": 1, "denseOperators": false, "newlineBeforeSemicolon": false}',
+      },
+    }
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "biome" },
+        javascriptreact = { "biome" },
+        json = { "biome" },
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        sql = { "sql_formatter" },
+        typescript = { "biome" },
+        typescriptreact = { "biome" },
+      },
+      format_on_save = { lsp_fallback = true },
+    })
+  end,
 }
