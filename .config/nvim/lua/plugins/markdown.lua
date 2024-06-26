7ce076c4d016abd499b345c4a647861a2ad71ceb
Doeke <doekebuursma@gmail.com>
2024-06-25 20:24:35 -0400

    Oops, still something there

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -5,12 +5,6 @@ return {
     local rm = require("render-markdown")
     rm.setup({
       headings = { "", "", "", "", "", "" },
-      -- checkbox = {
-      --   -- Character that will replace the [ ] in unchecked checkboxes
-      --   unchecked = "󰄱 ",
-      --   -- Character that will replace the [x] in checked checkboxes
-      --   checked = "󰱒 ",
-      -- },
       highlights = { heading = { backgrounds = { "DiffChange", "DiffChange", "DiffChange" } } },
     })
     vim.keymap.set("n", "<leader>tm", rm.toggle)
