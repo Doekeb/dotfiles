f3995961cb1a1da9e61749418a9d047871252baf
Doeke <doekebuursma@gmail.com>
2024-06-25 20:24:01 -0400

    Make markdown slightly prettier

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -3,7 +3,16 @@ return {
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   config = function()
     local rm = require("render-markdown")
-    rm.setup({})
+    rm.setup({
+      headings = { "", "", "", "", "", "" },
+      -- checkbox = {
+      --   -- Character that will replace the [ ] in unchecked checkboxes
+      --   unchecked = "󰄱 ",
+      --   -- Character that will replace the [x] in checked checkboxes
+      --   checked = "󰱒 ",
+      -- },
+      highlights = { heading = { backgrounds = { "DiffChange", "DiffChange", "DiffChange" } } },
+    })
     vim.keymap.set("n", "<leader>tm", rm.toggle)
   end,
 }
