e20d9c8bc87822c8d121009702a30b01170dcb50
doeke <doekebuursam@gmail.com>
2024-07-22 10:53:17 -0400

    Rip out null-ls, configure venv linters for python linters

--- .config/nvim/lua/plugins/language/mason.lua
+++ .config/nvim/lua/plugins/language/mason.lua
@@ -6,7 +6,7 @@ return {
     require("mason-lspconfig").setup({})
     local tools = {}
     vim.list_extend(tools, { "basedpyright", "bashls", "biome", "lua_ls", "pylsp", "pyright", "ruff_lsp", "taplo" }) -- lsp tools
-    vim.list_extend(tools, { "flake8", "mypy", "pylint" }) -- null_ls tools
+    vim.list_extend(tools, { "flake8", "mypy", "pylint" }) -- nvim-lint tools
     vim.list_extend(tools, { "biome", "black", "isort", "sql-formatter", "stylua" }) -- conform tools
     require("mason-tool-installer").setup({ ensure_installed = tools })
   end,
--- .config/nvim/lua/plugins/language/venv-selector.lua
+++ .config/nvim/lua/plugins/language/venv-selector.lua
@@ -1,66 +1,13 @@
-local venv_tool_path = function(venv_path, tool_name)
-  local path = venv_path .. "/bin/" .. tool_name
-  if os.rename(path, path) then
-    return path
-  end
-end
-
-local null_ls_python_venv_tool_config = function(venv_path, tool_name)
-  local config = {
-    cwd = function()
-      return vim.fn.getcwd()
-    end,
-  }
-  local command = venv_path .. "/bin/" .. tool_name
-  if os.rename(command, command) then
-    config.command = command
-    config.name = tool_name .. " (venv)"
-  end
-  return config
-end
-
 return {
   "linux-cultist/venv-selector.nvim",
-  dependencies = { "neovim/nvim-lspconfig", "nvimtools/none-ls.nvim", "nvim-telescope/telescope.nvim" },
+  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
   config = function()
-    local null_ls_hook = function(venv_path, _)
-      local null_ls = require("null-ls")
-      -- Disable python tools so we can re-register them using correct working directory and executable
-      local python_venv_sources = {}
-      for k, source in ipairs(null_ls.python_sources) do
-        null_ls.disable(source.name)
-        python_venv_sources[k] = source.with(null_ls_python_venv_tool_config(venv_path, source.name))
-      end
-      null_ls.register({
-        sources = python_venv_sources,
-      })
-    end
-
-    local pylsp_hook = function(venv_path, _)
-      local lspconfig = require("lspconfig")
-      local pylsp_config = { settings = { pylsp = { plugins = { jedi = { environment = venv_path } } } } }
-      lspconfig.pylsp_config = vim.tbl_deep_extend("force", lspconfig.pylsp_config or {}, pylsp_config)
-
-      -- local flake8_path = venv_tool_path(venv_path, "flake8")
-      -- if flake8_path then
-      --   lspconfig.pylsp_config.settings.pylsp.plugins.flake8 = lspconfig.pylsp_config.settings.pylsp.plugins.flake8 or {}
-      --   lspconfig.pylsp_config.settings.pylsp.plugins.flake8.enabled = true
-      --   lspconfig.pylsp_config.settings.pylsp.plugins.flake8.executable = flake8_path
-      -- end
-
-      lspconfig.pylsp.setup(lspconfig.pylsp_config)
-    end
-
     require("venv-selector").setup({
       name = { "venv", ".venv", "env" }, -- Remove "env" eventually
       changed_venv_hooks = {
-        null_ls_hook,
-        -- pylsp_hook,
         require("venv-selector").hooks.basedpyright,
-        -- require("venv-selector").hooks.pyright,
       },
     })
-
     vim.api.nvim_create_autocmd("VimEnter", { command = "VenvSelectCached" })
   end,
   event = "VeryLazy",
