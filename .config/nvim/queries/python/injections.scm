2c6ea1d55833f3082c7965b85d0f9ebe00a537be
doeke <doekebuursam@gmail.com>
2024-10-25 12:24:12 -0400

    Use a better magic comment setup (magic comment inside the string)

--- .config/nvim/queries/python/injections.scm
+++ .config/nvim/queries/python/injections.scm
@@ -1,7 +1,12 @@
-(
-  (comment) @magic_comment (#match? @magic_comment "!sql")
-  (string
-    (string_content) @injection.content (#set! injection.language "sql")
-  )
-)
+; This one isn't all that great due to the string possibly being a descendant of the next sibling.
+; (
+;   (comment) @magic_comment (#match? @magic_comment "!sql")
+;   .
+;   (string
+;     (string_content) @injection.content (#set! injection.language "sql")
+;   )
+; )
 
+(
+  (string_content) @injection.content (#match? @injection.content "-- !sql") (#set! injection.language "sql")
+)
