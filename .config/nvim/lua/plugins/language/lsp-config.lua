dea012799f86b95a5a756bdff8d18a3be9f36bca
Doeke <doekebuursma@gmail.com>
2024-12-11 09:59:09 -0500

    Swap neodev for lazydev

--- .config/nvim/lua/plugins/language/lsp-config.lua
+++ .config/nvim/lua/plugins/language/lsp-config.lua
@@ -1,8 +1,19 @@
 return {
   "neovim/nvim-lspconfig",
-  dependencies = { "folke/neodev.nvim" },
+  dependencies = {
+    {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+    },
+  },
   config = function()
-    require("neodev").setup({})
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
     local lspconfig = require("lspconfig")
     local on_attach = function(client, bufnr)
