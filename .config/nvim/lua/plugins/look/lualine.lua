dd24da76c3ecd2a2cbe0fbbfb82d65c731487335
doeke <doekebuursam@gmail.com>
2024-12-11 22:30:27 -0500

    Improve lualine symbols

--- .config/nvim/lua/plugins/look/lualine.lua
+++ .config/nvim/lua/plugins/look/lualine.lua
@@ -21,22 +21,22 @@ return {
           "filename",
           path = 1,
           symbols = {
-            modified = "[+]", -- Text to show when the file is modified.
-            readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
-            unnamed = "[No Name]", -- Text to show for unnamed buffers.
-            newfile = "[New]", -- Text to show for newly created file before first write
+            modified = "", -- Text to show when the file is modified.
+            readonly = "", -- Text to show when the file is non-modifiable or readonly.
+            unnamed = "󰊠", -- Text to show for unnamed buffers.
+            newfile = "", -- Text to show for newly created file before first write
           },
         },
       },
+      lualine_c = { "filetype" },
       lualine_x = {
-        "diagnostics",
         function()
           return replace(replace(require("venv-selector").venv(), vim.fn.getcwd(), "."), vim.fn.getenv("HOME"), "~")
             or "no virtual environment"
         end,
+        { "diagnostics", symbols = { error = " ", warn = " ", info = " ", hint = " " } },
       },
-      lualine_c = { "filetype" },
-      lualine_y = { "diff", "branch" },
+      lualine_y = { { "diff", symbols = { added = " ", modified = " ", removed = " " } }, "branch" },
       lualine_z = { "location" },
     },
   },
