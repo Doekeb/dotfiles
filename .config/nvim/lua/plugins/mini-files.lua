5e75a975ae4fffcd9eaef6ba08d5ac08b6e58241
Doeke <doekebuursma@gmail.com>
2025-01-24 08:58:38 -0500

    Toggle keymap for mini files

--- .config/nvim/lua/plugins/mini-files.lua
+++ .config/nvim/lua/plugins/mini-files.lua
@@ -4,6 +4,10 @@ return {
   config = function()
     local mini_files = require("mini.files")
     mini_files.setup({ windows = { preview = true } })
-    vim.keymap.set("n", "-", mini_files.open, { noremap = true })
+    vim.keymap.set("n", "-", function(...)
+      if not mini_files.close() then
+        mini_files.open(...)
+      end
+    end, { noremap = true })
   end,
 }
