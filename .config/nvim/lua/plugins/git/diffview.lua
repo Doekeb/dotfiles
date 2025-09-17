8264f8dd8e3b7e77696c8afc2169c1889e6e6a47
Doeke <doekebuursma@gmail.com>
2025-09-17 16:23:13 -0400

    Use diffview instead of fugitive for merge tool.

--- .config/nvim/lua/plugins/git/diffview.lua
+++ .config/nvim/lua/plugins/git/diffview.lua
@@ -2,7 +2,13 @@ return {
   "sindrets/diffview.nvim",
   config = function()
     local diffview = require("diffview")
-    diffview.setup({ file_panel = { win_config = { position = "bottom", height = 16 } } })
+    diffview.setup({
+      file_panel = { win_config = { position = "bottom", height = 16 } },
+      view = { merge_tool = { layout = "diff4_mixed" } },
+    })
+    vim.keymap.set("n", "<leader>do", function()
+      diffview.open({})
+    end, { noremap = true, silent = true })
     vim.keymap.set("n", "<leader>dc", function()
       diffview.close()
     end, { noremap = true, silent = true })
--- .config/nvim/lua/plugins/git/fugitive.lua
+++ .config/nvim/lua/plugins/git/fugitive.lua
@@ -1,11 +1,6 @@
 return {
   "tpope/vim-fugitive",
   config = function()
-    -- require("fugitive").setup({})
-    vim.api.nvim_create_user_command("Gmergesplit", function(opts)
-      vim.cmd(":Gvdiffsplit!")
-      vim.cmd(":wincmd J")
-    end, {})
     vim.keymap.set("n", "<leader>G", "<cmd>G<cr>", { noremap = true, silent = true })
   end,
 }
