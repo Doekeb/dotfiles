6795d02f245781c0e0263837fda6b649ca86cd07
Doeke <doekebuursma@gmail.com>
2025-11-19 15:20:36 -0500

    Swap most of telescope for snacks pickers.

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -39,6 +39,7 @@ return {
     require("snacks").setup({
       picker = {
         enabled = true,
+        ui_select = true,
         layouts = {
           select_r = select_r,
           dropdown_r = dropdown_r,
@@ -69,5 +70,35 @@ return {
         },
       },
     })
+    -- TODO: How to deal with pickers that don't need a preview window
+    vim.keymap.set("n", "<leader>ff", Snacks.picker.files, { desc = "[f]ind [f]iles" })
+    vim.keymap.set("n", "<leader>fF", function()
+      Snacks.picker.files({ hidden = true, ignored = true, follow = true })
+    end, { desc = "[f]ind all [F]iles" })
+    vim.keymap.set("n", "<leader>fif", Snacks.picker.grep, { desc = "[f]ind [i]n [f]iles" })
+    vim.keymap.set("n", "<leader>fiF", function()
+      Snacks.picker.grep({ hidden = true, ignored = true, follow = true })
+    end, { desc = "[f]ind [i]n all [F]iles" })
+    vim.keymap.set("n", "<leader>fb", Snacks.picker.buffers, { desc = "[f]ind [b]uffers" })
+    vim.keymap.set("n", "<leader>fib", Snacks.picker.grep_buffers, { desc = "[f]ind [i]n [b]uffers" })
+    -- vim.keymap.set("n", '<leader>f"', function()
+    --   Snacks.picker.registers({ layout = { preset = "select", layout = { relative = "cursor" } } })
+    -- end, { desc = '[f]ind registers ["]' })
+    vim.keymap.set("n", '<leader>f"', Snacks.picker.registers, { desc = '[f]ind registers ["]' })
+    vim.keymap.set("n", "<leader>f/", Snacks.picker.search_history, { desc = "[f]ind search history [/]" })
+    vim.keymap.set("n", "<leader>f:", Snacks.picker.command_history, { desc = "[f]ind command history [:]" })
+    vim.keymap.set("n", "<leader>fc", Snacks.picker.commands, { desc = "[f]ind [c]ommands" })
+    vim.keymap.set("n", "<leader>fe", Snacks.picker.diagnostics_buffer, { desc = "[f]ind [e]rrors" })
+    vim.keymap.set("n", "<leader>fE", Snacks.picker.diagnostics, { desc = "[f]ind all [E]rrors" })
+    vim.keymap.set("n", "<leader>fh", Snacks.picker.help, { desc = "[f]ind [h]elp" })
+    vim.keymap.set("n", "<leader>fj", Snacks.picker.jumps, { desc = "[f]ind [j]umps" })
+    vim.keymap.set("n", "<leader>fp", Snacks.picker.projects, { desc = "[f]ind [p]rojects" })
+    vim.keymap.set("n", "<leader>fu", Snacks.picker.undo, { desc = "[f]ind [u]ndo tree" })
+
+    vim.keymap.set("n", "<leader>fgS", Snacks.picker.git_stash, { desc = "[f]ind [g]it [S]tash" })
+    vim.keymap.set("n", "<leader>fgs", Snacks.picker.git_status, { desc = "[f]ind [g]it [s]tatus" })
+    vim.keymap.set("n", "<leader>fgl", Snacks.picker.git_log_line, { desc = "[f]ind [g]it [l]ine commits" })
+    vim.keymap.set("n", "<leader>fgf", Snacks.picker.git_log_file, { desc = "[f]ind [g]it [f]ile commits" })
+    vim.keymap.set("n", "<leader>fgc", Snacks.picker.git_log, { desc = "[f]ind [g]it [c]ommits" })
   end,
 }
