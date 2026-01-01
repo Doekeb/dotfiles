0020f3bb6bd2bad2d66cfe6336a8358b6812bfcc
doeke <doekebuursma@gmail.com>
2026-01-01 17:03:31 -0500

    Don't use mason if we are on Nix.

--- .config/nvim/lua/plugins/language/mason.lua
+++ .config/nvim/lua/plugins/language/mason.lua
@@ -1,27 +1,40 @@
-return {
-  "WhoIsSethDaniel/mason-tool-installer.nvim",
-  dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
-  config = function()
-    require("mason").setup({})
-    require("mason-lspconfig").setup({ automatic_enable = false })
-    local tools = {}
-    vim.list_extend(tools, {
-      "basedpyright",
-      "bashls",
-      "biome",
-      "deno",
-      "lua_ls",
-      "pylsp",
-      "pyright",
-      "ruff",
-      "taplo",
-      "ty",
-      "vimls",
-      "vtsls",
-    }) -- lsp tools
-    vim.list_extend(tools, { "flake8", "mypy", "pylint", "sqlfluff" }) -- nvim-lint tools
-    vim.list_extend(tools, { "biome", "black", "isort", "sqlfluff", "stylua" }) -- conform tools
-    vim.list_extend(tools, { "yq", "yaml-language-server", "cronstrue" }) -- bespoke tools
-    require("mason-tool-installer").setup({ ensure_installed = tools })
-  end,
-}
+local file = io.open('/etc/lsb-release', 'r')
+local is_nix
+for line in file:lines() do
+  if line:match('DISTRIB_ID=(.*)') == 'nixos' then
+    is_nix = true
+  end
+end
+
+if is_nix then
+  vim.print("Nix detected - no Mason")
+  return {}
+else
+  return {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
+    config = function()
+      require("mason").setup({})
+      require("mason-lspconfig").setup({ automatic_enable = false })
+      local tools = {}
+      vim.list_extend(tools, {
+        "basedpyright",
+        "bashls",
+        "biome",
+        "deno",
+        "lua_ls",
+        "pylsp",
+        "pyright",
+        "ruff",
+        "taplo",
+        "ty",
+        "vimls",
+        "vtsls",
+      }) -- lsp tools
+      vim.list_extend(tools, { "flake8", "mypy", "pylint", "sqlfluff" }) -- nvim-lint tools
+      vim.list_extend(tools, { "biome", "black", "isort", "sqlfluff", "stylua" }) -- conform tools
+      vim.list_extend(tools, { "yq", "yaml-language-server", "cronstrue" }) -- bespoke tools
+      require("mason-tool-installer").setup({ ensure_installed = tools })
+    end,
+  }
+end
