5750f7f96917d7242711e3e6a75671affcc5b4de
doeke <doekebuursma@gmail.com>
2026-01-04 12:35:31 -0500

    Use nix-installed treesitter grammars and regular neovim config otherwise.

--- .config/nixos/system/neovim.nix
+++ .config/nixos/system/neovim.nix
@@ -1,45 +1,27 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   programs.neovim = {
     enable = true;
 
-	#    configure.customLuaRC =
-	#      let
-	#        grammarsPath = pkgs.symlinkJoin {
-	#          name = "nvim-treesitter-grammars";
-	#          paths = pkgs.vimPlugins.nvim-treesitter.withAllGrammars.dependencies;
-	#        };
-	#      in
-	#      ''
-	#        -- also make sure to append treesitter since it bundles some languages
-	# vim.print("${pkgs.vimPlugins.nvim-treesitter}")
-	#        vim.opt.runtimepath:append("${pkgs.vimPlugins.nvim-treesitter}")
-	#        -- append all *.so files
-	# vim.print("${grammarsPath}")
-	#        vim.opt.runtimepath:append("${grammarsPath}")
-	#      '';
+    configure.customLuaRC =
+      let
+        grammarsPath = pkgs.symlinkJoin {
+          name = "nvim-treesitter-grammars";
+          paths = pkgs.vimPlugins.nvim-treesitter.withAllGrammars.dependencies;
+        };
+      in
+      ''
+        -- make sure the real init file gets sourced
+        -- vim.print(os.getenv("HOME") .. "/.config/nvim/init.lua")
+        dofile(os.getenv("HOME") .. "/.config/nvim/init.lua")
 
-    # configure.packages.myPlugins = with pkgs.vimPlugins; {
-    #   start = [
-    #     (nvim-treesitter.withPlugins (plugins: with plugins; [ nix ]))
-    #   ];
-    # };
+        -- also make sure to append treesitter since it bundles some languages
+        -- vim.print("${pkgs.vimPlugins.nvim-treesitter}")
+        vim.opt.runtimepath:append("${pkgs.vimPlugins.nvim-treesitter}")
 
-    # extraLuaConfig =
-    #   # lua
-    #   ''
-    #     require("lazy").setup()
-    #   '';
-
-    # configure.packages.myVimPackage = with pkgs.vimPlugins; {
-    #   start = [
-    #     (nvim-treesitter.withPlugins (plugins: with plugins; [ nix ]))
-    #   ];
-    # };
-
-    # plugins = [
-    #   pkgs.vimPlugins.nvim-treesitter.withAllGrammars
-    # ];
-    # extraPackages = with pkgs; [ gcc ];
+        -- append all *.so files
+        -- vim.print("${grammarsPath}")
+        vim.opt.runtimepath:append("${grammarsPath}")
+      '';
   };
 }
