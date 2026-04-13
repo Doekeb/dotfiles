862ccb8da9fb3c9893c078b2823a6f3ec536989f
Doeke <doekebuursma@gmail.com>
2026-04-13 16:43:01 -0400

    Add codecompanion filetype to markdown extension.

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,9 +1,11 @@
 return {
   "MeanderingProgrammer/markdown.nvim",
   -- dependencies = { "nvim-treesitter/nvim-treesitter" },
+  ft = { "markdown", "codecompanion" },
   config = function()
     local rm = require("render-markdown")
     rm.setup({
+      file_types = { "markdown", "codecompanion" },
       headings = { "", "", "", "", "", "" },
       highlights = { heading = { backgrounds = { "DiffChange", "DiffChange", "DiffChange" } } },
     })
