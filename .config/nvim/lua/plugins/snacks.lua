d5aac2b23e88dfd7783a45a0f5fa046dc222fe01
Doeke <doekebuursma@gmail.com>
2026-04-15 10:54:05 -0400

    Configure snacks input.

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -80,7 +80,49 @@ return {
 
     snacks.setup({
       styles = {},
-      input = { enabled = true, prompt_pos = "left", icon_pos = "title", win = { style = "blame_line" } },
+      input = {
+        enabled = true,
+        prompt_pos = "title",
+        icon_pos = false,
+        win = {
+          backdrop = false,
+          position = "float",
+          border = true,
+          title_pos = "left",
+          height = 1,
+          width = 60,
+          relative = "editor",
+          noautocmd = true,
+          row = -2,
+          col = 0,
+          -- row = 20,
+          -- relative = "cursor",
+          -- row = -3,
+          -- col = 0,
+          wo = {
+            winhighlight = "NormalFloat:SnacksInputNormal,FloatBorder:SnacksInputBorder,FloatTitle:SnacksInputTitle",
+            cursorline = false,
+          },
+          bo = {
+            filetype = "snacks_input",
+            buftype = "prompt",
+          },
+          --- buffer local variables
+          b = {
+            completion = false, -- disable blink completions in input
+          },
+          keys = {
+            n_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "n", expr = true },
+            i_esc = { "<esc>", { "cmp_close", "stopinsert" }, mode = "i", expr = true },
+            i_cr = { "<cr>", { "cmp_accept", "confirm" }, mode = { "i", "n" }, expr = true },
+            i_tab = { "<tab>", { "cmp_select_next", "cmp" }, mode = "i", expr = true },
+            i_ctrl_w = { "<c-w>", "<c-s-w>", mode = "i", expr = true },
+            i_up = { "<up>", { "hist_up" }, mode = { "i", "n" } },
+            i_down = { "<down>", { "hist_down" }, mode = { "i", "n" } },
+            q = "cancel",
+          },
+        },
+      },
       picker = {
         enabled = true,
         ui_select = true,
